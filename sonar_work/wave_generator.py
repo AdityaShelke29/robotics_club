b0de399ad52288cf75f54e81e38746287f64b89e
AdityaShelke29 <adityashelke2014@gmail.com>
2023-02-10 14:36:42 -0500

    cleaning up fourier_analysis.ipynb

--- sonar_work/wave_generator.py
+++ sonar_work/wave_generator.py
@@ -8,28 +8,26 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 
 
-def create_wave():
-    print("hello world")
+def create_wave(num_data_points, sampling_frequency, waveAdder):
 
-# Parameters 
-N = 1000000
-sampling_frequency = 500000
-sampling_time = N / sampling_frequency
+    # Used Parameters 
+    # num_data_points, sampling_frequency, waveAdder are all used directly in the function. 
+    # The waveAdder is in the format of [frequency, magnitude, starting time]
 
-# Time Array 
-time_array = np.arange(N)
-time_array = time_array / sampling_frequency
+    # Unused Parameters 
+    sampling_time = num_data_points / sampling_frequency
 
-# The waveAdder is in the format of [frequency, magnitude, starting time]
-waveAdder = [[50000, 1, 0.5], [20000, 2, 1]]
+    # Time Array 
+    time_array = np.arange(num_data_points)
+    time_array = time_array / sampling_frequency
 
-sample = np.zeros(N)
+    sample = np.zeros(num_data_points)
 
-for wave in waveAdder:
-    for i in range(N):
-        if (i / sampling_frequency < wave[2]):
-            sample[i] = sample[i] + 0
-        else:
-            sample[i] = sample[i] + wave[1] * np.sin(2 * math.pi * wave[0] * (i / sampling_frequency - wave[2]))
+    for wave in waveAdder:
+        for i in range(num_data_points):
+            if (i / sampling_frequency < wave[2]):
+                sample[i] = sample[i] + 0
+            else:
+                sample[i] = sample[i] + wave[1] * np.sin(2 * math.pi * wave[0] * (i / sampling_frequency - wave[2]))
 
-plt.plot(time_array, sample)
+    return [time_array, sample]
